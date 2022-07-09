9028fb79a1eb45a3334f981ebc30f149d5846cfa
邓银 <w-dengyin>
2022-07-09 08:21:24 +0800

    feat: auto href

--- packages/antd-mobile-ui-docs/src/hooks/useHref.ts
+++ packages/antd-mobile-ui-docs/src/hooks/useHref.ts
@@ -6,20 +6,11 @@ export default function () {
     // http://localhost:8000/~demos/button-demo1
     // http://192.168.0.166:10086/#/~demos/button-demo1
 
-    // https://antd-mobile-ui.setmp.com/~demos/button-demo1
-    // https://antd-mobile-ui.setmp.com/#/~demos/button-demo1
+    // https://antd-mobile-ui.setmp.com/~demos/button-demo2
+    // https://www.setmp.com/#/~demos/button-demo1
     let rst = href
       .replace('http://localhost:8000/', 'http://192.168.0.166:10086/#/')
-      .replace(
-        'https://antd-mobile-ui.setmp.com',
-        'https://antd-mobile-ui.setmp.com/#/'
-      )
-    //if (href.indexOf('8000') > -1) {
-    //switch (href) {
-    //case 'http://localhost:8000/~demos/button-demo1':
-    //rst = 'http://192.168.0.166:10086/#/demos/button/index'
-    //}
-    //}
+      .replace('https://antd-mobile-ui.setmp.com', 'https://www.setmp.com/#/')
     if (rst) {
       document.location.href = rst
     }
