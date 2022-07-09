1a3f432de00d6370253012232f615078af52b527
邓银 <w-dengyin>
2022-07-09 08:28:21 +0800

    feat: auto href

--- packages/antd-mobile-ui-docs/src/hooks/useHref.ts
+++ packages/antd-mobile-ui-docs/src/hooks/useHref.ts
@@ -10,7 +10,7 @@ export default function () {
     // https://www.setmp.com/#/~demos/button-demo1
     let rst = href
       .replace('http://localhost:8000/', 'http://192.168.0.166:10086/#/')
-      .replace('https://antd-mobile-ui.setmp.com', 'https://www.setmp.com/#/')
+      .replace('https://antd-mobile-ui.setmp.com/', 'https://www.setmp.com/#/')
     if (rst) {
       document.location.href = rst
     }
